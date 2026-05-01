7ae0fe4673912487b4dfab532908307d34f1ba42
milyons <malcolmlyons@unomaha.edu>
2026-04-30 22:42:11 -0500

    terraform stage 1 azure resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,17 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "acmp-rg"
+  name     = "acmp2400-rg"
   location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acmpacr${random_integer.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "random_integer" "suffix" {
+  min = 1000
+  max = 9999
 }
